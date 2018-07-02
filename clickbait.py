0abe0dfc7324b72bc517b59834daac29b35c6238
imperialself <kevin.w.eales@gmail.com>
2018-07-01 20:58:21 -0700

    Main file outline

---
+++ clickbait.py
@@ -0,0 +1,9 @@
+import random
+
+#A bunch of lists containing parts of speech go here
+
+#A bunch of functions containing template headlines that will pull random parts of speech into them, Madlibs style
+
+#A single function that will randomly select a template above
+
+#Run function
